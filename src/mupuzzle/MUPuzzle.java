6b3c5f883c96dff7745227f72a0d44a02ddb1619
Victor <Daniel@Air-de-Daniel.lan>
2018-01-28 03:13:03 -0600

    Obtener demostración de teorema
    Se hizo una función que genera los pasos para llegar a la cadena deseada, sino manda mensaje de que no se pudo

--- src/mupuzzle/MUPuzzle.java
+++ src/mupuzzle/MUPuzzle.java
@@ -5,113 +5,160 @@
  */
 package mupuzzle;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Daniel
  */
 public class MUPuzzle {
     
-    String str="MI"; 
+    private ArrayList<String> arrStr=new ArrayList();
 
     public MUPuzzle() {
     }
 
-    public String getStr() {
-        return str;
-    }
-
-    public void setStr(String str) {
-        this.str = str;
+    public String getArrStr() {
+        String res="";
+        
+        for (int i = arrStr.size()-1; i >= 0; i--) {
+            res+=arrStr.get(i);
+            res+="\n";
+        }
+        
+        return res;
     }
     
-    public boolean addU(){
-        
-        int size=str.length();
-        char lastChar=str.charAt(size-1);
+    //Primera regla: Agrega U
+    public String regla1(String axioma){
+        return axioma+'U';
+    }
+    
+    public boolean checaRegla1(String axioma){
+        int size=axioma.length();
+        char lastChar=axioma.charAt(size-1);
         if(lastChar=='I'){
-            str=str+'U';
             return true;
         }
         
         return false;
     }
     
-    public boolean duplicate(){
-        String dup=str.substring(1);
+    public String regla2(String axioma){
+        String dup=axioma.substring(1);
         
-        if(!dup.isEmpty()){
-            str=str+dup;
+        return axioma+dup;
+    }
+    
+    public boolean checaRegla2(String axioma){
+        String fst2=axioma.substring(1,3);
+        String lst2=axioma.substring(axioma.length()-2);
+        
+        if(axioma.length()>4&&((fst2.compareTo("UU")==0&&lst2.compareTo("UU")==0)||
+                (fst2.compareTo("UI")==0&&lst2.compareTo("IU")==0)))
             return true;
-        }
-        
-        return false;
-    }
-    
-    public boolean changeI2U(int inicio, int fin){
-        String strI=str.substring(inicio, fin);
-        
-        if(strI.length()==3 && strI.compareTo("III")==0){
-            str=str.substring(0, inicio)+'U'+str.substring(fin, str.length());
+        if((fst2.compareTo("II")==0&&lst2.compareTo("UI")==0)||(fst2.compareTo("IU")==0&&lst2.compareTo("II")==0)||(fst2.compareTo("II")==0&&lst2.compareTo("II")==0))
             return true;
-        }
-        
         return false;
-    }
-    
-    public boolean delU(int inicio, int fin){
-        String strU=str.substring(inicio, fin);
-        
-        if(strU.length()==2 && strU.compareTo("UU")==0){
-            str=str.substring(0, inicio)+str.substring(fin, str.length());
-            return true;
-        }
-        
-        return false;
-    }
-    
-    public void buildStr(String strReq){
-        buildStr(strReq, "MI");
-    }
-    
-    private void buildStr(String strReq, String strActual){
-        if(strReq.compareTo(strActual)==0)
-            return;
-        
-        
     }
 
+    public String regla3(String axioma){
+
+        return axioma.replaceFirst("III", "U");
+    }
+    
+    public boolean checaRegla3(String axioma){
+        return axioma.contains("III");
+    }
+    
+    public String regla4(String axioma){
+        
+        return axioma.replaceFirst("UU", "");
+    }
+    
+    public boolean checaRegla4(String axioma){
+        return axioma.contains("UU");
+    }
+    
+    public boolean checaI(String str){
+        String strTmp=str.replaceAll("III", "");
+        if(str.length()>strTmp.length())
+            return true;
+        return false;
+    }
+    
+    public boolean checaU(String str){
+        String strTmp=str.replaceAll("UU", "");
+        if(str.length()>strTmp.length())
+            return true;
+        return false;
+    }
+    
+    public boolean demuestraTeo(String teo, String axioma){
+        
+        if(axioma.compareTo(teo)==0){
+            arrStr.add(axioma);
+            return true;
+        }
+        
+        boolean status=false;
+        
+        // Ver qué reglas aplicar
+        if(!status&&this.checaRegla1(axioma)) //Regla 1
+            status=this.demuestraTeo(teo, this.regla1(axioma));
+        if(!status&&this.checaRegla3(axioma)) //Regla 3
+            status=this.demuestraTeo(teo, this.regla3(axioma));
+        if(!status&&this.checaRegla4(axioma))
+            status=this.demuestraTeo(teo, this.regla4(axioma));
+        if(!status&&(axioma.compareTo("MI")==0||this.checaRegla2(axioma))) //Regla 2
+            status=this.demuestraTeo(teo, this.regla2(axioma));
+        
+        if(status)
+            arrStr.add(axioma);
+        
+        return status;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         MUPuzzle mu=new MUPuzzle();
         
-        System.out.println(mu.getStr());
-        //        MI
-        mu.duplicate();
-        System.out.println(mu.getStr());
-        //        MII
-        mu.duplicate();
-        System.out.println(mu.getStr());
-        //        MIIII
-        if(mu.changeI2U(1, 4))
-            System.out.println(mu.getStr());
-        else
-            System.out.println("Error");
-        //        MUI
-        mu.addU();
-        System.out.println(mu.getStr());
-        //        MUIU
-        mu.duplicate();
-        System.out.println(mu.getStr());
-        //        MUIUUIU
-        
-        if(mu.delU(3, 5))
-            System.out.println(mu.getStr());
-        else
-            System.out.println("Error");
-        //        MUIIU
+//        System.out.println(mu.getAxioma());
+//        //        MI
+//        mu.duplicate();
+//        System.out.println(mu.getAxioma());
+//        //        MII
+//        mu.duplicate();
+//        System.out.println(mu.getAxioma());
+//        //        MIIII
+//        if(mu.changeI2U())
+//            System.out.println(mu.getAxioma());
+//        else
+//            System.out.println("Error");
+//        //        MUI
+//        mu.addU();
+//        System.out.println(mu.getAxioma());
+//        //        MUIU
+//        mu.duplicate();
+//        System.out.println(mu.getAxioma());
+//        //        MUIUUIU
+//        
+//        if(mu.delU())
+//            System.out.println(mu.getAxioma());
+//        else
+//            System.out.println("Error");
+//        //        MUIIU
 
+        //Probrando el último método
+        try{
+           mu.demuestraTeo("MUIIU", "MI");
+           System.out.println("Listo");
+           System.out.println(mu.getArrStr());
+        }catch(StackOverflowError e){
+            System.out.println("No se pudo encontrar solución");
+        }
+        
     }
     
 }
